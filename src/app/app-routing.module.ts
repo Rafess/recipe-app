0867f2e7224e721b573320f63d1b552466f10986
Rafes <rafadvcoliveira@gmail.com>
2021-12-03 11:29:26 -0300

    Change recipes folder disposition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // "/home" = RecipeListComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
